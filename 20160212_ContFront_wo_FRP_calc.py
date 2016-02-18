d82ccb2024b12c3ce60e7aceb7db02ac512eeeb2
Kirsten Barrett <kirsten.barrett@le.ac.uk>
2016-02-18 14:35:03 +0000

    now w satellite (filename

--- 20160212_ContFront_wo_FRP_calc.py
+++ 20160212_ContFront_wo_FRP_calc.py
@@ -15,18 +15,7 @@ def do_kdtree(prevDetects,newXY):
     dist = tree.query(YXnew)[0]
     return dist
 
-
-#AK BOREAL EXTENT
-##minX = -511738.931
-##minY = 1176158.734
-##maxX = 672884.463
-##maxY = 2117721.949
 distTolerance = 2 #pixels between front HPs (residual burning)
-
-##inProj = Proj(init='epsg:4326') #GEOGRAPHIC WGS84
-##outProj = Proj(init='esri:102006') #AK ALBERS EQUAL AREA CONIC
-##nProjRows = np.int_(np.rint((maxY-minY)/1000))
-##nProjCols = np.int_(np.rint((maxX-minX)/1000))
 
 
 os.chdir('/Users/kirsten/Documents/data/MODIS/FRE_TEST_DATA')
@@ -38,7 +27,7 @@ filList = os.listdir('.')
 ##ASSIGN CONT/FRONT ACCORDINGLY
 ##OUTPUT TO A NEW CSV FILE
 
-#newCSV = #...
+
 jDayList = []
 csvList = []
 minJday = 365
@@ -65,43 +54,66 @@ for jDay in jDayList:
     jDayYs = np.asarray([])
     jDayProjXs = np.asarray([])
     jDayProjYs = np.asarray([])
+    jDayAreas = np.asarray([])
     jDayFRPs = np.asarray([])
+    jDayFRPsareas = np.asarray([])
     jDayHrs = np.asarray([])
     jDayMints = np.asarray([])
     jDayJdays = np.asarray([])
     jDayYrs = np.asarray([])
     jDaySats = np.asarray([])
-    jDayConfs = np.asarray([])
-    jDayData = np.asarray([])
-    #...
+    jDayFils = np.asarray([])
+    #FRPx,FRPy,FRPxProj,FRPyProj,Area,FRP,FrpArea,hrs,mints,js,yrs,filNams
+    
     for oP in ovrPassFilList:
         oPdata = genfromtxt(oP, delimiter=',')
         if np.size(oPdata)>0:
-            oPxs = oPdata[:,2]
+            oPxs = oPdata[:,0]
             jDayXs = np.append(jDayXs,oPxs)
-            oPys = oPdata[:,3]
+            oPys = oPdata[:,1]
             jDayYs = np.append(jDayYs,oPys)
             oPprojXs = oPdata[:,2]
             jDayProjXs = np.append(jDayProjXs,oPprojXs)
             oPprojYs = oPdata[:,3]
             jDayProjYs = np.append(jDayProjYs,oPprojYs)
+            oPareas = oPdata[:,4]
+            jDayAreas = np.append(jDayAreas,oPareas)
             opFRPs = oPdata[:,5]
             jDayFRPs = np.append(jDayFRPs,opFRPs)
+            opFRPareas = oPdata[:,6]
+            jDayFRPsareas = np.append(jDayFRPsareas,opFRPareas)
+            opHrs = oPdata[:,7]
+            jDayHrs = np.append(jDayHrs,opHrs)
+            opMints = oPdata[:,8]
+            jDayMints = np.append(jDayMints,opMints)
+            opJdays = oPdata[:,9]
+            jDayJdays = np.append(jDayJdays,opJdays)
+            opYrs = oPdata[:,10]
+            jDayYrs = np.append(jDayYrs,opYrs)
             
-        #    opFRPs = ...
-            #ALL OTHER HP INFO...
     opXY = np.asarray((jDayXs,jDayYs))
     if jDay == minJday:
         prevDetects = opXY
     else:
         dists = do_kdtree(prevDetects,opXY)
-        exportCSV = np.column_stack([jDayXs,jDayYs,dists,jDayProjXs,jDayProjYs,jDayFRPs])
-        np.savetxt(str(jDay)+'contFront_XY_proj_crds.csv', exportCSV, delimiter=",")
+        filName = np.array(np.repeat(oP,len(dists)))
+        exportCSV = np.column_stack([jDayXs,jDayYs,jDayProjXs,jDayProjYs,jDayAreas,jDayFRPs,jDayFRPsareas,jDayHrs,jDayMints,jDayJdays,jDayYrs,filName,dists])
+        np.savetxt(str(jDay)+'contFront_XY_proj_crds.csv', exportCSV, delimiter=",",fmt = '%s')
         
-        #    opNewData = ###COMBINE IT ALL TOGETHER
-        ##ADD TO jDayData
+##ADD HEADER TO FILES
+filList = os.listdir('.')
+hdr = 'X,Y,AEA_AK_X,AEA_AK_Y,Area,FRP,FrpArea,hr,min,julian,year,fileName,dist_to_closest\n'
+for filnam in filList:
+    if filnam[-8:] == 'crds.csv':
+        newfilnam = filnam.replace('.csv','_hdr.csv')
+        newfil = open(newfilnam,'w')
+        newfil.write(hdr)
+        fil = open(filnam,'r')
+        content = fil.read()
+        newfil.write(content)
+        newfil.close()
 
-     #THEN WRITE ALL jDayData TO newCSV
+    
 
         
             
